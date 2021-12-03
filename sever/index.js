bc017a892be220e7dd8837b5dee8fed017ee4d42
zyf <zyfeleven@gmail.com>
2021-12-03 15:48:27 -0500

    backend completed

--- sever/index.js
+++ sever/index.js
@@ -4,6 +4,8 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/message");
+const {loginRequired,ensureUserCorrect} = require("./middleware/auth");
 
 const PORT = 2222;
 
@@ -13,9 +15,24 @@ const errHandler = require("./handlers/error");
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use("api/auth", authRoutes);
-//TODOS: all routes config
 
+//TODOS: all routes config
+app.use("api/auth", authRoutes);
+app.use("api/users/:id/messages",loginRequired, ensureUserCorrect, messageRoutes);
+
+app.get("/api/messages", loginRequired, async function(req,res,next){
+    try{
+        let messages = await db.Message.find()
+        .sort({createAt:"desc"})
+        .populate("user", {
+            username:true,
+            profilePictureUrl:true,
+        });
+        return res.status(200).json(messages);
+    }catch(err){
+        return next(err);
+    }
+})
 //error handler
 app.use(function(req,res,next){
     let err = new Error("Not Found");
