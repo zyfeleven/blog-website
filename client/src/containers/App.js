73d5e08830e3ee45b55d3a125ed05dd3c2ce4003
zyf <zyfeleven@gmail.com>
2021-12-07 12:11:03 -0500

    an easy navbar

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -2,15 +2,16 @@ import React,{Component} from 'react';
 import {Provider} from "react-redux";
 import {configureStore} from "../store";
 import {BrowserRouter as Router} from "react-router-dom";
+import Navbar from './Navbar';
 
 const store = configureStore();
 
 const App = () => (
   <Provider store = {store}>
     <Router>
-      <h1>
-        hello world
-      </h1>
+      <div className="onboarding">
+        <Navbar></Navbar>
+      </div>
     </Router>
   </Provider>
 )
