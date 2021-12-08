512235fd045a87e3d488688d5dcfdbf9d6d26d11
yukino <86507968+zyfeleven@users.noreply.github.com>
2021-12-08 18:29:09 -0500

    Delete sever directory

--- sever/handlers/error.js
+++
@@ -1,8 +0,0 @@
-function errorHandler(error,req,res,next){
-    return res.status(error.status||500).json({
-        error:{
-            message:error.message||"Something went wrong."
-        }
-    })
-}
-module.exports = errorHandler;
